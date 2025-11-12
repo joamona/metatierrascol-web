13b265d2eca56324d45cade859f67e761e6fb6c0
joamona <joamona@cgf.upv.es>
2025-11-12 12:32:25 +0100

    Añadida configuración para compilar con gisserver: ng build --configuration gisserver

---
+++ src/environments/environment.gisserver.ts
@@ -0,0 +1,8 @@
+//environment for the server upvusig
+export const environment = {
+    apiUrl: 'https://metatierrascol.gisserver.car.upv.es/api/',
+    webUrl: 'https://metatierrascol.gisserver.car.upv.es/',
+    geoserverUrl: 'https://metatierrascol.gisserver.car.upv.es/geoserver/',
+    debug: false
+
+};
