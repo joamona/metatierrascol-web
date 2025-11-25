82cb6e84aa8c129fd60a9682f187bb59d85bfc97
joamona <joamona@cgf.upv.es>
2025-11-25 11:45:52 +0100

    Añadida configuración para trabajar con apache en docker apache-docker

---
+++ src/environments/environment.apache-docker.ts
@@ -0,0 +1,7 @@
+//environment for development
+export const environment = {
+    apiUrl:'http://localhost/api/',
+    webUrl:'http://localhost:/',
+    geoserverUrl:'http://localhost:8080/geoserver/',
+    debug: true
+};
